4b93747a8cf7a29277792f72f608f312a0f2b5c7
Dai Sheng <sheng.dai@aalto.fi>
2021-12-13 15:59:01 +0200

    modify the DGP

--- MC/MC.py
+++ MC/MC.py
@@ -6,20 +6,19 @@ from pystoned.constant import CET_ADDI, FUN_PROD, OPT_LOCAL, RTS_VRS
 from sklearn.svm import SVR
 
 
-def inputs(n, d, sig):
+def inputs(n, d, SNR):
 
-	x = np.random.uniform(low=1, high=10, size=(n, d))
-	nse = np.random.normal(0, sig, n)
+	x = np.random.uniform(low=-1, high=1, size=(n, d))
+	y_true = np.linalg.norm(x, axis=1)**2
 
-	if d == 1:
-		y_true = 3 + x[:,0]**0.5
-		y = y_true + nse
-	elif d == 2:
-		y_true = 3 + x[:,0]**0.2 + x[:,1]**0.3
-		y = y_true + nse
-	else:
-		y_true = 3 + x[:, 0]**0.05 + x[:, 1]**0.15 + x[:, 2]**0.3
-		y = y_true + nse
+	sigma = np.sqrt(np.var(y_true, ddof=1, axis=0)/SNR)
+	nse = np.random.normal(0, sigma, n)
+	
+	y = y_true + nse
+
+	# Standardization: zero mean and unit norm.
+	#X = (x - np.mean(x, axis=0))/np.linalg.norm(x, axis=0)
+	#Y = y / np.linalg.norm(y)
 
 	return x, y, y_true
 
