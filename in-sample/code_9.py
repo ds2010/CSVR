b1d5ae0db257bc3c4fa7c1143d4627b8c8d1f9f8
liao-zq <liaozhiqiang.m@gmail.com>
2022-09-27 10:45:53 +0300

    add MC simulations

---
+++ in-sample/code_9.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../functionall/')
+import numpy as np
+import pandas as pd
+import random
+import MC
+
+
+def main():
+
+    #simulations
+    re_all = []
+    for i in range(M):
+        re_all.append(MC.simulation(n, d, sig))
+
+    data = np.array(re_all)
+    df = pd.DataFrame(data, columns = ['csvr', 'svr', 'cnls', 'lcr'])
+    df.to_csv('mse' + '{0}_{1}_{2}.csv'.format(n, d, sig), index=False)
+
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+    random.seed(0)
+
+    M=50
+    n=50
+    d=3
+    sig = 2
+
+    main()
---
+++ out-of-sample/code_3.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../functionall/')
+import numpy as np
+import pandas as pd
+import random
+import MC
+
+
+def main():
+
+    #simulations
+    re_all = []
+    for _ in range(M):
+        re_all.append(MC.simulation_out(n, d, sig, nt))
+
+    data = np.array(re_all)
+    df = pd.DataFrame(data, columns = ['csvr', 'svr', 'cnls', 'lcr'])
+    df.to_csv('mse_' + '{0}_{1}_{2}.csv'.format(n, d, sig), index=False)
+
+
+if __name__ == '__main__':
+
+    np.random.seed(0)
+    random.seed(0)
+
+    M=50
+    n=500
+    d=5
+    sig = 2
+    nt = 1000
+
+    main()
